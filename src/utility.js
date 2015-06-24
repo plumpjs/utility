bd9aeb794cecc658879214d8d8a18dba6406eeb2
James Martin-Davies <devjmd@icloud.com>
2015-06-24 08:31:50 +0100

    Add throttle functionality.

--- src/utility.js
+++ src/utility.js
@@ -65,6 +65,26 @@
 
 		removeEvent: function(el, type, fn) {
 			return el.removeEventListener(type, fn, false);
+		},
+
+		throttle: function(fn, threshold, scope) {
+			threshold || (threshold = 250);
+			var last, deferTimer;
+			return function() {
+				var context = scope || this;
+				var now =+ new Date,
+					args = arguments;
+				if (last && now < last + threshold) {
+					clearTimeout(deferTimer);
+					deferTimer = setTimeout(function() {
+						last = now;
+						fn.apply(context, args);
+					}, threshold);
+				} else {
+					last = now;
+					fn.apply(context, args);
+				}
+			};
 		}
 	};
 }));
